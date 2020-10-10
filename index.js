4a1221ef180db889ef8e991d1268ea1b8fd45a38
nahid <nahidkishore99@gmail.com>
2020-10-11 03:20:27 +0600

    send appointment and save to database

--- index.js
+++ index.js
@@ -6,23 +6,30 @@ const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 
-const MongoClient = require('mongodb').MongoClient;
 app.use(cors());
 app.use(bodyParser.json());
+
+
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jolmh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 app.get('/', function (req, res) {
   res.send('hello world')
 })
 
-const uri = `mongodb+srv://${process.env.DB_USER}  :${process.env.DB_PASS}@cluster0.jolmh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true });
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true ,useUnifiedTopology: true });
+
 client.connect(err => {
   const appointmentCollection = client.db(process.env.DB_NAME).collection("appointments");
+
+
 app.post('/addAppointment',(req, res)=>{
   const appointment=req.body;
+  console.log(appointment);
   appointmentCollection.insertOne(appointment)
   .then(result=>{
-    res.send(result.insertedCount)
+    res.send(result.insertedCount>0)
   })
 })
 });        
