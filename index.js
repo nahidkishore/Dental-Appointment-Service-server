e31e9dfc1167b2ebac34f747054de8097266c39e
nahid <nahidkishore99@gmail.com>
2020-10-11 20:08:37 +0600

    all patients data load in client side

--- index.js
+++ index.js
@@ -31,6 +31,25 @@ app.post('/addAppointment',(req, res)=>{
   .then(result=>{
     res.send(result.insertedCount>0)
   })
+});
+
+//all patients list
+app.get('/AllPatients',(req,res)=>{
+  
+  appointmentCollection.find({})
+  .toArray((err,documents)=>{
+    res.send(documents);
+  })
+
+})
+
+app.post('/appointmentsByDate', (req, res) => {
+  const date = req.body;
+  console.log(date.date);
+  appointmentCollection.find({ date: date.date })
+      .toArray((err, documents) => {
+          res.send(documents);
+      })
 })
 });        
 
