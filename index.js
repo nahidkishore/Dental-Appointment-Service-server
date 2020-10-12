55029fdf3d234f9ddb2311c5bac025c44b2cc3a1
nahid <nahidkishore99@gmail.com>
2020-10-12 18:37:46 +0600

    display sensitive information to admin only

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require('express');
-const app = express();
+
 const port = 5000;
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 
-app.use(cors());
-app.use(bodyParser.json());
-
 
 const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jolmh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static('doctors'));
+app.use(fileUpload());
+
+
+
+
+
 
 app.get('/', function (req, res) {
   res.send('hello world')
@@ -22,7 +33,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true ,useUnifiedTopology:
 
 client.connect(err => {
   const appointmentCollection = client.db(process.env.DB_NAME).collection("appointments");
-
+  const doctorCollection = client.db(process.env.DB_NAME).collection("doctors");
 
 app.post('/addAppointment',(req, res)=>{
   const appointment=req.body;
@@ -45,12 +56,53 @@ app.get('/AllPatients',(req,res)=>{
 
 app.post('/appointmentsByDate', (req, res) => {
   const date = req.body;
-  console.log(date.date);
-  appointmentCollection.find({ date: date.date })
+  const email = req.body.email;
+  doctorCollection.find({ email: email })
+      .toArray((err, doctors) => {
+          const filter = { date: date.date }
+          if (doctors.length === 0) {
+              filter.email = email;
+          }
+          appointmentCollection.find(filter)
+              .toArray((err, documents) => {
+                  console.log(email, date.date, doctors, documents)
+                  res.send(documents);
+              })
+      })
+})
+
+
+app.get('/doctors', (req, res) => {
+  doctorCollection.find({})
       .toArray((err, documents) => {
           res.send(documents);
       })
 })
+
+
+
+app.post('/addADoctor', (req, res) => {
+  const file = req.files.file;
+  const name = req.body.name;
+  const email = req.body.email;
+  // console.log(file, name, email);
+  const newImg = file.data;
+  const encImg = newImg.toString('base64');
+
+  var image = {
+      contentType: req.files.file.mimetype,
+      size: req.files.file.size,
+      img: Buffer.from(encImg, 'base64')
+  };
+
+  doctorCollection.insertOne({ name, email, image })
+      .then(result => {
+          res.send(result.insertedCount > 0);
+      })
+})
+
+
+
 });        
 
 app.listen(process.env.PORT || port);
